1e89c261f4738cbc2ffacff21ebd8c356efbc780
santiagocaceres12 <85034080+santiagocaceres12@users.noreply.github.com>
2022-03-09 12:07:38 -0500

    Create mapper3.py

---
+++ Corte1/mapper-reducer/code/mapper3.py
@@ -0,0 +1,14 @@
+import sys
+
+precios=[]
+ciudades=[]
+valores=[]
+
+for line in sys.stdin:
+        precio=line.split(",")
+        precios.append(precio[1])
+        ciudades.append(precio[6])
+precios.pop(0)
+ciudades.pop(0)
+for preci, ciu  in zip(precios, ciudades):
+        print( ciu,":",preci)
